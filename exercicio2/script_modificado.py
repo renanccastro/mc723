1b596d136ed601df299a80470245966d26a69572
Renan Castro <renanccastro@gmail.com>
2016-04-06 15:18:43 -0300

    Adicionando arquivos

--- exercicio2/script_modificado.py
+++ exercicio2/script_modificado.py
@@ -8,7 +8,7 @@ def cmdline(command, cwd):
         shell=True,
         cwd = cwd
     )
-    return process.communicate()[0]
+    return process
 
 if __name__ == "__main__":
     # parameters=["-maxtrace", "20", -l1-isize 16K -l1-dsize 16K -l1-ibsize 32 -l1-dbsize 32]
@@ -17,10 +17,6 @@ if __name__ == "__main__":
     programs=['164.gzip.f2b','168.wupwise.f2b','171.swim.f2b','172.mgrid.f2b','173.applu.f2b','176.gcc.f2b']
     names=['gzip_f2b','wupwise_f2b','swim_f2b','mgrid_f2b','applu_f2b','gcc_f2b']
     
-    
-    p1 = Popen(["dmesg"], stdout=PIPE)
-	p2 = Popen(["grep", "hda"], stdin=p1.stdout, stdout=PIPE)
-	output = p2.communicate()[0]
     
     #Primeiro comparamos o tamanho da cache, e descobrimos o com menor tempo!
     poweroftwo = [ 4**j for j in range(0,6) ]
@@ -32,12 +28,16 @@ if __name__ == "__main__":
         for cacheisize in poweroftwofrom4:
             for cachedsize in poweroftwo:
                 #subprocess.cwd("%s/%s" % (location,val))
-                p1=cmdline((("/home/staff/lucas/mc723/dinero4sbc/dineroIV -informat s -trname %s -maxtrace 20 "
+                cwd=(("%s/%s") % (location,val))
+                string=(("/home/staff/lucas/mc723/dinero4sbc/dineroIV -informat s -trname %s -maxtrace 20 "
                                      "-l1-isize %dK -l1-dsize %dK "
                                      "-l1-ibsize %d -l1-dbsize %d") %
-                                     (names[idx], cacheisize, cachedsize, ibsize,dbsize)), (("%s/%s") % (location,val)))
-                p2=cmdline("grep \"Demand miss rate\" | awk -F ' ' '{print $4}' "
-                                     "| xargs -n2")
+                                     (names[idx], cacheisize, cachedsize, ibsize,dbsize))
+                p1=Popen(args=string, cwd=cwd, shell=True, stdout=PIPE)
+                p2=Popen(args="grep \"Demand miss rate\"", cwd=cwd, shell=True, stdout=PIPE, stdin=p1.stdout)
+                p3=Popen(args="awk -F ' ' '{print $4}' ", cwd=cwd, shell=True, stdout=PIPE, stdin=p2.stdout)
+                p4=Popen(args="xargs -n2", cwd=cwd, shell=True, stdout=PIPE, stdin=p3.stdout)
+                output=p4.communicate()[0]
                 print cacheisize, " ", cachedsize," ",output
                 print >> f, cacheisize, " ", cachedsize," ",output
                 # dineroIV -informat s -trname vortex_f2b -maxtrace 101 -l1-isize 16K -l1-dsize 16K -l1-ibsize 32 -l1-dbsize 32
