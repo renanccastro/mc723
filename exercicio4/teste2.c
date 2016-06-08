8299004d0cb373abd9b2c1563642fca705559738
Renan Castro <renanccastro@gmail.com>
2016-06-08 01:19:47 -0300

    adicionando fonte de teste

--- exercicio4/teste2.c
+++ exercicio4/teste2.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#define LOCK_ADDRESS 0x6400000
+#define ACQUIRE_GLOBAL_LOCK while(*((int*)LOCK_ADDRESS))
+#define RELEASE_GLOBAL_LOCK *((int*)LOCK_ADDRESS)=0
 
-int main(){
-
+volatile int procCounter = 0;
+void RecursiveHello(int n, int procNumber){
+  if(n){
+    printf("Hi from processor %d!\n", procNumber);
+    RecursiveHello(n - 1, procNumber);
+  }
+}
+int main(int ac, char *av[]){
+  int procNumber;
+  ACQUIRE_GLOBAL_LOCK;
+  procNumber = procCounter;
+  procCounter++;
+  RELEASE_GLOBAL_LOCK;
+  if(procNumber % 2) {
+    for (i=0;i<100000;i++);
+  }
+  RecursiveHello(10, procNumber);
+  exit(0);
+  return 0; 
 }
